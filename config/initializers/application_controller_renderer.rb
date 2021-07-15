45700e769fcd00461a846b8e9fecf3b79785bde8
nogamishinpei <nogamichiru@gmail.com>
2021-07-15 02:56:34 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b3e0e8d30d50b4c30806061efdde5397d9a7031b24f8f10462d5bc832caeb97e64c52cb8667592e7c64eb0c7501057170d1db09434769862bc95a920ce2f95b4'
